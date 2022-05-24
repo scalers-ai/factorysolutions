d9fd590f650d149bb5b562a9cb1c5cb3f030abf6
scalers-dev-prash <oneplusprabhu@gmail.com>
2022-05-24 14:27:50 +0530

    Updated the instructions to account for changes to the way in which we are initializing datasources and dashboards for grafana.

--- simulator.md
+++ simulator.md
@@ -5,18 +5,30 @@
     1. To install grafana on the Windows machine follow [this documentation](https://grafana.com/grafana/download/8.3.2?edition=oss&platform=windows) to download windows installer file and run it to install the application.
 
         > Note: Install Grafana version 8.3.2 with OSS Edition.
-
-    2. Open a **Command Prompt** as **Administrator**.
+                                                                                                                                                                                                                                     
+    2. Setting up grafana datasources, dashboards and telegraf configurations.
     
-    3. Run the below command to setup the Grafana configurations.
-
-        ```sh
-        cd <path to repo>\simulator_configs
-
-        setup_grafana.bat
-        ```
-
-
+        1. Run the command below to get the EFLOW VM IP on a power shell
+        
+            ```sh
+            Get-eflowVmAddr
+            ```
+    
+        2. Run the configuration script with the EFLOW IP from the previous step
+    
+            ```sh
+            cd <path to repo>\simulator_configs
+    
+            python3 configure.py -ip <EFLOW IP>
+            ```
+        
+        3. Run the below command to setup the grafana datasources and dashboards.
+        
+            ```sh
+            cd <path to repo>\simulator_config
+    
+            setup_grafana.bat
+            ```
 2. Setting up Telegraf on Windows
 
     1. Open a **Command Prompt** as an **Administrator**.
@@ -29,22 +41,7 @@
         setup_telegraf.bat
         ```
 
-    3. Run the command the below command to get the EFLOW VM IP on a power shell
-    
-        ```sh
-        Get-eflowVmAddr
-        ```
-
-    4. Run the configuration script with the EFLOW IP from the previous step
-
-        ```sh
-        cd <path to repo>\simulator_configs
-
-        python3 configure.py -ip <EFLOW IP>
-        ```
-
-
-    3. Run the below command to setup the Telegraf configuration file and create Grafana UI dashboards
+    3. Run the below command to setup the Telegraf configuration file
 
        ```sh
         cd "C:\Program Files\InfluxData\telegraf\"
@@ -56,7 +53,7 @@
 
 # Visualize UI Windows
 
-* Open the URL [localhost:3000](http://localhost:5000) on your Windows Deployment machine to open the Grafana dashboard.
+* Open the URL [localhost:3001](http://localhost:3001) on your Windows Deployment machine to open the Grafana dashboard.
 
 * Open the dashboard **Defect Detection - Windows** from the dashboard list to see Impeller Defect Detection view.
 
--- simulator_config/configure.py
+++ simulator_config/configure.py
@@ -32,13 +32,13 @@ def generate_key(grafana_ip: str) -> str:
     :return api_key: Grafana API key
     """
     json_key = {
-        "name": "apikey",
+        "name": "apikey4",
         "role": "Admin"
     }
 
     head = {'Content-type': 'application/json', 'Accept': 'text/plain'}
     resp = requests.post(
-        f'http://admin:admin@{grafana_ip}:3000/api/auth/keys',
+        f'http://admin:admin@{grafana_ip}:3001/api/auth/keys',
         json=json_key, headers=head
     )
 
@@ -53,81 +53,57 @@ def generate_key(grafana_ip: str) -> str:
 
 
 def configure_grafana(eflow_ip: str, json_path: str, grafana_ip: str):
-    # creat datasource
+
+    head = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+    #update datasource with eflow vm ip
     datasource_paths = {
-        os.path.join(json_path, 'influx_datasource.json'),
-        os.path.join(json_path, 'testdata_datasource.json'),
+        os.path.join(json_path, 'influx_datasource.yml')
     }
     for path in datasource_paths:
         with open(path, 'r') as source_file:
-            datasource = json.load(source_file)
-        head = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        if datasource["type"] != "testdata":
-            datasource["url"] = datasource["url"].replace("<eflowIP>", eflow_ip)
-        # datasource["url"] = f"http://{eflow_ip}:8086"
-        resp= requests.post(
-            f'http://admin:admin@{grafana_ip}:3000/api/datasources',
-            json=datasource, headers=head
-        )
+            datasource = source_file.read()
+            datasource = datasource.replace("EFLOWIP", eflow_ip)
 
-        if 200 != resp.status_code:
-            print("Unable to create the grafana datasource. Exiting")
-            exit(1)
+        with open(path, 'w') as source_file:
+            source_file.write(datasource)
 
-    print("Grafana datasource created.")
+    print("Grafana datasource file update with eflow vm ip.")
 
     time.sleep(10)
-# create dashboards
+    # update dashboards with eflow vm ip
     with open(os.path.join(json_path, 'defect_dashboard_windows.json'), 'r') as d_file:
         dashboard_data = d_file.read()
         dashboard_data = dashboard_data.replace("EFLOWIP", eflow_ip)
-        dashboard = json.loads(dashboard_data)
 
-    resp = requests.post(
-        f'http://admin:admin@{grafana_ip}:3000/api/dashboards/db',
-        json=dashboard, headers=head
-    )
+    with open(os.path.join(json_path, 'defect_dashboard_windows.json'), 'w') as d_file:
+        d_file.write(dashboard_data)
 
-    if 200 != resp.status_code:
-        print("Unable to create the grafana dashboard. Exiting")
-        exit(1)
-    
     with open(os.path.join(json_path, 'safety_dashboard_windows.json'), 'r') as d_file:
-
         dashboard_data = d_file.read()
         dashboard_data = dashboard_data.replace("EFLOWIP", eflow_ip)
-        dashboard = json.loads(dashboard_data)
 
-    resp = requests.post(
-        f'http://admin:admin@{grafana_ip}:3000/api/dashboards/db',
-        json=dashboard, headers=head
-    )
+    with open(os.path.join(json_path, 'safety_dashboard_windows.json'), 'w') as d_file:
+        d_file.write(dashboard_data)
 
-    if 200 != resp.status_code:
-        print("Unable to create the grafana dashboard. Exiting")
-        exit(1)
-
-    print("Grafana dashboards created.")
+    print("Grafana dashboards updated with eflow vm ip.")
 
 
-def update_telegraf(eflow_ip: str, config_path:str, api_key:str):
+def update_telegraf(eflow_ip: str, config_path:str):
     """
     Update telegraf config
 
     :param eflow_ip: EFLOW VM IP
     :param config_path : Telegraf config file path
-    :param api_key: Grafana API Key
     """
     telegraf_conf = os.path.join(config_path, 'telegraf.conf')
     with open(telegraf_conf, 'r') as conf_file:
         conf_data=conf_file.read()
-
-    conf_data = conf_data.replace('<EFLOW_IP>', eflow_ip)
-    updated_conf_data = conf_data.replace('<API_KEY>', api_key)
+        conf_data = conf_data.replace('EFLOWIP', eflow_ip)
 
     # Write the file out again
     with open(telegraf_conf, 'w') as conf_file:
-        conf_file.write(updated_conf_data)
+        conf_file.write(conf_data)
 
 
 def main():
@@ -138,15 +114,12 @@ def main():
     grafana_ip = "127.0.0.1"
     json_path = ".\\"
 
-    # generate grafana api key
-    api_key = generate_key(grafana_ip)
-
     # configure dashboard and datasource in grafana
     configure_grafana(eflow_ip, json_path, grafana_ip)
 
     # configure telegraf config file
     config_path = "C:\Program Files\InfluxData\\telegraf"
-    update_telegraf(eflow_ip, config_path, api_key)
+    update_telegraf(eflow_ip, config_path)
 
 
 if __name__ == "__main__":
